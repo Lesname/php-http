4efb11ca1fb0a210dc4f508240554863dc41136e
mkroeders <mkroeders@gmail.com>
2023-04-29 18:16:15 +0200

    Fix locale middleware factory

--- src/Middleware/Locale/LocaleMiddleware.php
+++ src/Middleware/Locale/LocaleMiddleware.php
@@ -1,9 +1,8 @@
 <?php
 declare(strict_types=1);
 
-namespace LessHttp\Middleware;
+namespace LessHttp\Middleware\Locale;
 
-use SplQueue;
 use SplPriorityQueue;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
