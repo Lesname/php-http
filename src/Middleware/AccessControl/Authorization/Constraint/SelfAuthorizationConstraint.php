1a853f4e61374e0a7e4fb24c3405ed9146dff8d2
mkroeders <mkroeders@gmail.com>
2026-08-21 13:11:36 +0200

    Fix missing override

--- src/Middleware/AccessControl/Authorization/Constraint/SelfAuthorizationConstraint.php
+++ src/Middleware/AccessControl/Authorization/Constraint/SelfAuthorizationConstraint.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace LesHttp\Middleware\AccessControl\Authorization\Constraint;
 
+use Override;
 use Psr\Http\Message\ServerRequestInterface;
 use LesValueObject\Composite\ForeignReference;
 
 final class SelfAuthorizationConstraint implements AuthorizationConstraint
 {
+    #[Override]
     public function isAllowed(ServerRequestInterface $request): bool
     {
         $identityReference = $request->getAttribute('identity.reference');
